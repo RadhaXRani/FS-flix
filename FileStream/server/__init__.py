ebf53bb748b49011addfb800e6b0e446ebc55e20
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-07-28 00:33:12 +0530

    Update __init__.py

--- FileStream/server/__init__.py
+++ FileStream/server/__init__.py
@@ -5,3 +5,8 @@ def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+
+@routes.get("/health", allow_head=True)
+async def health_check_handler(_):
+    return web.Response(status=200)
+
